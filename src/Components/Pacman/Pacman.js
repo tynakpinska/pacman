c45194ed06e5b8f45907df690ac868cca6c4095f
tynakpinska <m.k.kepinska@gmail.com>
2020-09-26 11:40:21 +0200

    add pacman direction

--- src/Components/Pacman/Pacman.js
+++ src/Components/Pacman/Pacman.js
@@ -1,10 +1,50 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
+import { ReactComponent as PacmanSvg } from "./pacman.svg";
 import "./Pacman.css";
 
-const Pacman = () => {
-  return <div className='pacman'>Pacman</div>;
-};
+class Pacman extends Component {
+  state = {
+    direction: "right",
+    position: {
+      top: 50,
+      left: 50,
+    },
+  };
+  constructor(props) {
+    super(props);
+    this.pacmanRef = React.createRef();
+  }
 
-Pacman.defaultProps = {};
+  componentDidMount() {
+    this.pacmanRef.current.focus();
+  }
+
+  handleKeyDown = e => {
+    console.log(e.keyCode, e.key);
+  };
+
+  render() {
+    const { direction, position } = this.state;
+    return (
+      <div
+        ref={this.pacmanRef}
+        className={`pacman pacman-${direction}`}
+        tabIndex="0"
+        style={position}
+        onKeyDown={this.handleKeyDown}
+      >
+        <PacmanSvg />
+      </div>
+    );
+  }
+}
+
+Pacman.defaultProps = {
+  step: 50, // 50px
+  size: 50, // pacman size: 50px x 50 px
+  // TODO: move to conifg
+  border: 10 * 2,
+  topScoreBoardHeight: 50,
+};
 
 export default Pacman;
