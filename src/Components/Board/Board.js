ae58db1c28d30a4edf46187ce8961925dc05989b
tynakpinska <m.k.kepinska@gmail.com>
2020-09-26 13:53:33 +0200

    place food

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,26 +1,71 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Board.css";
 import Pacman from "../Pacman/Pacman";
 import Ghost from "../Ghost/Ghost";
 import Food from "../Food/Food";
 
-const Board = () => {
-  return (
-    <div className="board">
-      <Food position={{ top: 300, left: 50 }} />
-      <Food position={{ top: 100, left: 100 }} />
-      <Food position={{ top: 200, left: 200 }} />
-      <Pacman />
-      {/* <Ghost color="red" />
-      <Ghost color="yellow" />
-      <Ghost color="blue" /> */}
-      <Ghost color="pink" />
-    </div>
-  );
-};
+class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.pacmanRef = React.createRef();
+    this.foods = [];
+    this.amoundOfFood =
+      ((window.innerWidth - this.props.foodSize) *
+        (window.innerHeight - this.props.topScoreBoardHeight)) /
+        (this.props.foodSize * this.props.foodSize) -
+      1;
+    for (let i = 0; i < this.amoundOfFood; i++) {
+      this["food" + i] = React.createRef();
+    }
+  }
 
+  lookFor = () => {
+    
+  };
+
+  render() {
+    const { foodSize, border, topScoreBoardHeight } = this.props;
+    let foods = [];
+    let currentTop = 0;
+    let currentLeft = 1 * foodSize;
+    for (let i = 0; i < this.amoundOfFood; i++) {
+      if (currentLeft + foodSize >= window.innerWidth - border) {
+        currentTop += foodSize;
+        currentLeft = 0;
+      }
+      if (
+        currentTop + foodSize >=
+        window.innerHeight - border - topScoreBoardHeight
+      ) {
+        break;
+      }
+      const position = { left: currentLeft, top: currentTop };
+      currentLeft += foodSize;
+      foods.push(
+        <Food
+          key={`food-elem-${i}`}
+          position={position}
+          ref={this["food" + 1]}
+        />
+      );
+    }
+    return (
+      <div className="board">
+        {foods}
+        <Pacman ref={this.pacmanRef} />
+        {/* <Ghost color="red" />
+        <Ghost color="yellow" />
+        <Ghost color="blue" /> */}
+        <Ghost color="pink" />
+      </div>
+    );
+  }
+}
+//TODO: move to config
 Board.defaultProps = {
-  score: 0,
+  foodSize: 50,
+  border: 10 * 2,
+  topScoreBoardHeight: 50,
 };
 
 export default Board;
